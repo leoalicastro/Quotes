bc338824fd863c3b61de55dc986143c618cbc93c
leoalicastro <78497745+leoalicastro@users.noreply.github.com>
2021-08-24 09:35:39 -0600

    Update

--- Books copy/books/settings.py
+++ Books copy/books/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+SECRET_KEY = '+8&lve@n-k(cw!pp=i^h0uq=i0yuz#(4gqx^8j%m732swv1kd8'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
